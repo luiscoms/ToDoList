a7745d4e05eddd931846eae00cb68895015abf67
Luis Com S <dev@luiscoms.com.br>
2014-06-10 10:02:17 -0300

    Adicionado layout da todolist ao home

--- src/com/projeto/bean/PropertyManager.java
+++ src/com/projeto/bean/PropertyManager.java
@@ -5,7 +5,9 @@ import java.util.Date;
 import java.util.List;
 
 import javax.ejb.EJB;
+import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
 
 import com.projeto.ejb.SingletonBean;
 import com.projeto.model.Property;
@@ -13,53 +15,60 @@ import com.projeto.model.Property;
 @ManagedBean(name="manager")
 public class PropertyManager {
 
-    @EJB
-    SingletonBean ejb;
+	@EJB
+	SingletonBean ejb;
 
-    ArrayList<Property>  cacheList  = new ArrayList();
+	ArrayList<Property>  cacheList  = new ArrayList();
 
-    private Property selectedProp;
-    private String key;
-    private String value;
-    private Date data;
+	private Property selectedProp;
+	private String key;
+	private String value;
+	private Date data;
 
 	public void save() {
-        ejb.put(data, key, value);
-    }
+		FacesMessage message = new FacesMessage("Task registered");
+		FacesContext.getCurrentInstance().addMessage(null, message);
 
-    public void clear() {
-        ejb.delete(selectedProp);
-    }
-    public List getCacheList() {
-        return ejb.getCache();
-    }
+		ejb.put(data, key, value);
+	}
 
-    // Getters and Setters
-    
-    public Property getSelectedProp() {
-        return selectedProp;
-    }
+	public void clear() {
+		FacesMessage message = new FacesMessage("Task deleted");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+		ejb.delete(selectedProp);
+	}
+	public List getCacheList() {
+		return ejb.getCache();
+	}
 
-    public void setSelectedProp(Property selectedProp) {
-        this.selectedProp = selectedProp;
-    }
-    public String getKey() {
-        return key;
-    }
+	// Getters and Setters
 
-    public void setKey(String key) {
-        this.key = key;
-    }
+	public Property getSelectedProp() {
+		return selectedProp;
+	}
 
-    public String getValue() {
-        return value;
-    }
+	public void setSelectedProp(Property selectedProp) {
+		FacesMessage message = new FacesMessage("Task selected");
+		FacesContext.getCurrentInstance().addMessage(null, message);
+		this.selectedProp = selectedProp;
+	}
+	public String getKey() {
+		return key;
+	}
 
-    public void setValue(String value) {
-        this.value = value;
-    }
+	public void setKey(String key) {
+		this.key = key;
+	}
 
-    public Date getData() {
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+
+	public Date getData() {
 		return data;
 	}
 
