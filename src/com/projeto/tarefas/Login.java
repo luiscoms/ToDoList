c1cafceb55d4708aa4860aaa4479286a08891d37
Luis Com S <dev@luiscoms.com.br>
2014-06-16 22:53:50 -0300

    Adicionado redirect para home

--- src/com/projeto/tarefas/Login.java
+++ src/com/projeto/tarefas/Login.java
@@ -1,37 +1,42 @@
 package com.projeto.tarefas;
 
 import java.io.Serializable;
+
 import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
 import javax.inject.Named;
 
 @Named("login")
 @SessionScoped
-public class Login implements Serializable{
-    String userName = "";
-    String password = "";
+public class Login implements Serializable {
+	String userName = "";
+	String password = "";
 
-    public String getUserName() {
-        return userName;
-    }
+	public String getUserName() {
+		return userName;
+	}
 
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
 
-    public String getPassword() {
-        return password;
-    }
+	public String getPassword() {
+		return password;
+	}
 
-    public void setPassword(String password) {
-        this.password = password;
-    }
-    
-    public String logar(){
-        if ("teste".equalsIgnoreCase(userName)){
-            return "index";
-        } else{        
-        return "";
-        }
-    }
-    
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String logar(){
+		if ("teste".equalsIgnoreCase(userName)){
+			return "home";
+		} else {
+			FacesMessage message = new FacesMessage("Invalid user/pass");
+			FacesContext.getCurrentInstance().addMessage(null, message);
+			return "";
+		}
+	}
+
 }
